7bedd4f647d355caa2d55f4b0a8a030ba7351b98
LastCow9000 <sys19912002@hanmail.net>
2021-04-04 00:42:21 +0900

    boj 1182 직접구현

--- Algorithm/BOJ/1182_부분수열의 합_s2/1182.py
+++ Algorithm/BOJ/1182_부분수열의 합_s2/1182.py
@@ -1,6 +1,6 @@
 # boj 1182 부분수열의 합 s2
 # noj.am/1182
-
+'''
 import itertools
 
 N, S = map(int, input().split())
@@ -13,6 +13,24 @@ for i in range(1, N + 1):
     for comb in itertools.combinations(_int, i):
         if sum(comb) == S:
             cnt += 1
-
+'''
 
 # 직접 구현해보기
+N, S = map(int, input().split())
+_int = list(map(int, input().split()))
+cnt = 0
+
+
+def recursive(idx, sum):
+    global cnt
+    if idx >= N:
+        return
+    if sum == S:
+        cnt += 1
+
+    recursive(idx + 1, sum)
+    recursive(idx + 1, sum + _int[idx])
+
+
+recursive(0, 0)
+print(cnt)
