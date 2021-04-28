59f6e7b3a0ce16f40c59c6ae7bff47a727b2d0e1
LastCow9000 <sys19912002@hanmail.net>
2021-04-29 00:23:41 +0900

    boj 5671 eof upd

--- Algorithm/BOJ/5671_호텔 방 번호_s4/5671.py
+++ Algorithm/BOJ/5671_호텔 방 번호_s4/5671.py
@@ -1,18 +1,20 @@
 # boj 5671 호텔 방 번호 s4
 # noj.am/5671
 
-N, M = map(int, input().split())
-cnt = 0
-flag = True
+while True:
+    try:
+        cnt = 0
+        N, M = map(int, input().split())
 
-for num in range(N, M + 1):
-    length = len(str(num))
-    for i in range(length):
-        if str(num)[0] == str(num)[i]:
-            flag = False
-            break
-    if flag:
-        cnt += 1
+        for num in range(N, M + 1):
+            for i in range(len(str(num))):
+                # 숫자를 문자로 변경 후 특정 문자가 문자열에 2개이상 존재하는 경우 반복되므로 no count
+                if str(num).count(str(num)[i]) > 1:
+                    break
+            else:  # break가 안되고 for문이 종료되면 실행
+                cnt += 1
 
-    flag = True
-print(cnt)
+        print(cnt)
+
+    except EOFError:
+        break
