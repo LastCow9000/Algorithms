f6cf58dcd23b3b8dd922f9de1725e9e7236fe716
LastCow9000 <sys19912002@hanmail.net>
2021-05-12 23:46:00 +0900

    boj 10866 sol

---
+++ Algorithm/BOJ/10866_덱_s4/10866.py
@@ -0,0 +1,42 @@
+# boj 10828 스택 s4
+# noj.am/10828
+
+from collections import deque
+import sys
+
+inp = sys.stdin.readline
+dq = deque()
+for _ in range(int(inp())):
+    command = inp().rstrip().split()
+
+    if command[0] == "push_front":
+        dq.appendleft(command[1])
+    elif command[0] == "push_back":
+        dq.append(command[1])
+    elif command[0] == "pop_front":
+        if dq:
+            print(dq.popleft())
+        else:
+            print(-1)
+    elif command[0] == "pop_back":
+        if dq:
+            print(dq.pop())
+        else:
+            print(-1)
+    elif command[0] == "size":
+        print(len(dq))
+    elif command[0] == "empty":
+        if dq:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == "front":
+        if dq:
+            print(dq[0])
+        else:
+            print(-1)
+    elif command[0] == "back":
+        if dq:
+            print(dq[-1])
+        else:
+            print(-1)
