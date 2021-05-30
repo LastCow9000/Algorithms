7ab794e2bab4d78355451338db2e393a1f3abcd4
LastCow9000 <sys19912002@hanmail.net>
2021-05-31 00:06:12 +0900

    boj 2960 오타 fix

--- Algorithm/BOJ/2960_에라토스테네스의 체_s4/2960.py
+++ Algorithm/BOJ/2960_에라토스테네스의 체_s4/2960.py
@@ -1,3 +1,17 @@
 # boj 2960 에라토스테네스의 체 s4
 # noj.am/2960
 
+N, K = map(int, input().split())
+
+visited = [False, False] + [True] * (N - 1)
+cnt = 0
+
+for i in range(2, N + 1):
+    if visited[i]:
+        for j in range(i, N + 1 ,i):
+            if visited[j]:
+                cnt += 1
+            visited[j] = False
+            if cnt == K:
+                print(j)
+                break
