dc02561e6dc381cf1651b7ffe6d21bd869dd4496
LastCow9000 <sys19912002@hanmail.net>
2021-10-01 23:24:37 +0900

    bubble sort impl

---
+++ DataStructure/Sorting/bubble.py
@@ -0,0 +1,11 @@
+# O(N^2)
+# Stable
+
+nums = list(map(int, input().split()))
+
+for i in range(len(nums) - 1):
+    for j in range(len(nums) - 1 - i):
+        if nums[j] > nums[j + 1]:
+            nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+print(nums)
